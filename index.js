bee3d4e57bca6452d3705a27eaec3ab8587546c9
jpdcorte <jensjoris.decorte@ugent.be>
2020-03-23 16:40:52 +0100

    Voorbeeldoplossing

--- index.js
+++ index.js
@@ -1,55 +1,188 @@
-// Import and create the Express application
-var express = require('express');
+// Creëer Express applicatie
+var express = require("express");
 var app = express();
+const path = require("path");
 
-// Initialize sqlite database
-const dbFile = "./sqlite.db";
+// Initialiseer de database
+const dbFile = path.join("./sqlite.db");
 const fs = require("fs");
+fs.unlinkSync(dbFile); // uncomment deze lijn tijdelijk als je de database wilt verwijderen
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
 
-// If ./sqlite.db does not exist, create it
+// Creëer de database tabellen
+// Goede bron voor SQLite naslag: https://www.sqlitetutorial.net/
 db.serialize(() => {
   if (!exists) {
     db.run(
-      "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)"
+      "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);"
     );
-    console.log("Database was created");
+    db.run(
+      "CREATE TABLE Notes (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, userId INTEGER, FOREIGN KEY(userId) REFERENCES Users(id));"
+    );
   }
 });
 
-// Respond with "Hello World" on the homepage
-app.get('/', function (req, res) {
-    res.send("hello world")
-})
+// Voorbeeld van een html response met Express (ter illustratie)
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
-// Respond with a list of all users in the database
-app.get('/users', (req, res) => {
-    db.all("SELECT * from Users", (err, rows) => {
-        res.send(JSON.stringify(rows));
-    });
-})
-
-// http://localhost:8081/add?name=Tom will add user with name Tom to the database
-// Respond with result message
-app.get('/add', (req, res) => {
-    let name = req.query.name;
-    if(name) {
-        db.run(`INSERT INTO Users (name) VALUES (?)`, name, error => {
-            if (error) {
-              res.send({ message: "error!" });
-            } else {
-                res.send({ message: "added " + name });
-            }
-        });
-    } else {
-        res.send({ message: "No user was added, add a name parameter to the url like so: `http://.../add?name=Tom`" });
+// Antwoord met een lijst van alle gebruikers
+app.get("/users", (req, res) => {
+  db.all("SELECT * from Users", (err, rows) => {
+    if (err) {
+      res.send({ error: err });
+      return;
     }
-})
+    res.send(JSON.stringify(rows));
+  });
+});
 
-var server = app.listen(8081, function () {
-   var port = server.address().port
-   
-   console.log("Example app listening on port %s", port)
-})
+// Voeg een gebruiker toe aan de database
+app.get("/add", (req, res) => {
+  // controleer of de name parameter is meegegeven
+  let name = req.query.name;
+  if (!name) {
+    res.send({ error: "No name argument found" });
+    return;
+  }
+
+  db.all(`SELECT name from Users WHERE name Like (?)`, name, (err, rows) => {
+    // return eventuele errors
+    if (err) {
+      res.send({ error: err });
+      return;
+    }
+    if (rows.length > 0) {
+      res.send({ error: "User already exists" });
+      return;
+    }
+
+    db.run(`INSERT INTO Users (name) VALUES (?)`, name, error => {
+      if (error) {
+        res.send({ error: error });
+        return;
+      }
+      res.send({ success: `Successfully added ${name}` });
+    });
+  });
+});
+
+// Voeg een notitie toe
+app.get("/addnote", (req, res) => {
+  // controleer of de parameters meegegeven zijn
+  let name = req.query.name;
+  let content = req.query.content;
+  if (!name || !content) {
+    res.send({ error: "Parameters name and content are required" });
+    return;
+  }
+
+  // haal de gebruiker op uit de database
+  db.get(`SELECT id FROM Users WHERE name LIKE ?`, [name], (err, row) => {
+    // return een eventuele error
+    if (err) {
+      res.send({ error: err });
+      return;
+    }
+    if (!row) {
+      res.send({ error: `No user named ${name}` });
+      return;
+    }
+
+    // haal alle notities van de gebruiker op
+    let userId = row.id;
+    db.run(
+      `INSERT INTO Notes(content, userId) VALUES(?, ?)`,
+      [content, userId],
+      err => {
+        if (err) {
+          res.send({ error: err });
+          return;
+        }
+        res.send({ success: "Inserted note" });
+      }
+    );
+  });
+});
+
+// Antwoord met een lijst van alle notities van een gebruiker
+app.get("/notes", (req, res) => {
+  // controleer of de naam parameter is meegegeven
+  let name = req.query.name;
+  if (!name) {
+    res.send({ error: "No name argument found" });
+    return;
+  }
+
+  // haal de userId van de gebruiker op
+  db.get(`SELECT id from Users WHERE name LIKE (?)`, name, (err, row) => {
+    // return bij eventuele error
+    if (err) {
+      res.send({ error: err });
+      return;
+    }
+    // controleer of een gebruiker met de naam bestaat
+    if (!row) {
+      res.send({ error: `No user named ${name}` });
+      return;
+    }
+
+    // haal alle notities van de gebruiker op
+    let userId = row.id;
+    db.all("SELECT * from Notes WHERE userId = ?", userId, (err, rows) => {
+      if (err) {
+        res.send({ error: err });
+        return;
+      }
+      res.send(JSON.stringify(rows));
+    });
+  });
+});
+
+// Verwijdert gebruiker met diens notities
+app.get("/remove", (req, res) => {
+  // controleer of de naam parameter is meegegeven
+  let name = req.query.name;
+  if (!name) {
+    res.send({ error: "No name argument found" });
+    return;
+  }
+
+  db.get(`SELECT id FROM Users WHERE name LIKE ?`, [name], (err, row) => {
+    // return bij eventuele error
+    if (err) {
+      res.send({ error: err });
+      return;
+    }
+    // controleer of een gebruiker met de naam bestaat
+    if (!row) {
+      res.send({ error: `No user named ${name}` });
+      return;
+    }
+
+    // verwijder notities van de gebruiker
+    let userId = row.id;
+    db.run(`DELETE FROM Notes WHERE userId = (?)`, [userId], err => {
+      if (err) {
+        res.send({ error: err });
+        return;
+      }
+    });
+    // verwijder de gebruiker
+    db.run(`DELETE FROM Users WHERE id = (?)`, [userId], err => {
+      if (err) {
+        res.send({ error: err });
+        return;
+      }
+      res.send({ success: "Deleted successfully" });
+    });
+  });
+});
+
+var server = app.listen(8081, function() {
+  var port = server.address().port;
+  console.log("Example app listening on port %s", port);
+});
